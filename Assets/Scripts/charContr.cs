bbd2e0b43c44479e39aafed35dcf7f856ca2041d
MYruh2 <melihhokelekli76@gmail.com>
2025-06-11 16:32:18 +0300

    yarak

--- Assets/Scripts/charContr.cs
+++ Assets/Scripts/charContr.cs
@@ -1,6 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
 
-public class charContr : MonoBehaviour
+
+public class CharContr : MonoBehaviour
 {
     Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    Vector2 movement;
+    public Animator Anim;
+
+    bool facingRight = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+
+        if (movement.y != 0 || movement.x != 0)
+        {
+            Anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            Anim.SetBool("isRunning", false);
+        }
+
+
+        if (movement.x < 0 && facingRight)
+        {
+            flip();
+            facingRight = !facingRight;
+        }
+
+        else if(movement.x > 0 && !facingRight)
+        {
+            flip();
+            facingRight = !facingRight;
+        }
+    }
+
+    void flip()
+    {
+        transform.Rotate(0, 180f, 0);
+    }
+
+
 }
